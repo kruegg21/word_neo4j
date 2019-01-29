8d3253ac12303ae62fe81baa327c68631444b757
kruegg21 <parallels@debian-gnu-linux-vm.localdomain>
2019-01-28 16:12:15 -0800

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kruegg21/word_neo4j',
-    version='1.0.3',
+    version='1.0.4',
     zip_safe=False,
 )
